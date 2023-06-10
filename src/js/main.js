bcc6560121da2348787d4aaa7e1e3d1b920c4774
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-06-10 15:09:11 +0200

    Add switch

--- src/js/main.js
+++ src/js/main.js
@@ -23,20 +23,24 @@ const haushaltsbuch = {
 
   eintrag_ausgeben() {
     console.log(`Titel: ${this.neuer_eintrag.titel}
-  Typ: ${this.neuer_eintrag.typ}
-  Betrag: ${this.neuer_eintrag.betrag} ct
-  Datum: ${this.neuer_eintrag.datum}`);
+Typ: ${this.neuer_eintrag.typ}
+Betrag: ${this.neuer_eintrag.betrag} ct
+Datum: ${this.neuer_eintrag.datum}`);
   },
 
   eintrag_mit_gesamtbilanz_verrechnen() {
-    if (this.neuer_eintrag.typ === "Einnahme") {
-      this.gesamtbilanz.einnahmen += this.neuer_eintrag.betrag;
-      this.gesamtbilanz.bilanz += this.neuer_eintrag.betrag;
-    } else if (this.neuer_eintrag.typ === "Ausgabe") {
-      this.gesamtbilanz.ausgaben += this.neuer_eintrag.betrag;
-      this.gesamtbilanz.bilanz -= this.neuer_eintrag.betrag;
-    } else {
-      console.log(`Der Typ "${this.neuer_eintrag.typ}" ist nicht bekannt.`);
+    switch (this.neuer_eintrag.typ) {
+      case "Einnahme":
+        this.gesamtbilanz.einnahmen += this.neuer_eintrag.betrag;
+        this.gesamtbilanz.bilanz += this.neuer_eintrag.betrag;
+        break;
+      case "Ausgabe":
+        this.gesamtbilanz.ausgaben += this.neuer_eintrag.betrag;
+        this.gesamtbilanz.bilanz -= this.neuer_eintrag.betrag;
+        break;
+      default:
+        console.log(`Der Typ "${this.neuer_eintrag.typ}" ist nicht bekannt.`);
+        break;
     }
   },
 
